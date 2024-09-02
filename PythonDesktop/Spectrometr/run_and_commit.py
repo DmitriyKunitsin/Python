b649ed41857430692e27f54f5dc32abfff02f390
Куницин Дмитрий Сергеевич <d.kunicin@looch.local>
2024-09-02 14:36:41 +0700

    Автоматическое обновление после выполнения скрипта.

--- PythonDesktop/Spectrometr/run_and_commit.py
+++ PythonDesktop/Spectrometr/run_and_commit.py
@@ -1,22 +1,25 @@
 import subprocess
 import os
-
+class Colors:
+    RED = '\033[91m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    BLUE = '\033[94m'
+    RESET = '\033[0m'
 def run_script(script_path):
-    """Запускает указанный скрипт."""
     try:
         subprocess.run(['python', script_path], check=True)
-        print(f"Скрипт {script_path} успешно выполнен.")
+        print(f"{Colors.GREEN}Скрипт {script_path} успешно выполнен.{Colors.RESET}")
     except subprocess.CalledProcessError as e:
-        print(f"Ошибка при выполнении скрипта {script_path}: {e}")
+        print(f"{Colors.RED}Ошибка при выполнении скрипта {script_path}: {e}{Colors.RESET}")
 
 def git_add_and_commit(commit_message):
-    """Добавляет изменения в Git и делает коммит."""
     try:
         subprocess.run(['git', 'add', '.'], check=True)
         subprocess.run(['git', 'commit', '-m', commit_message], check=True)
-        print("Изменения успешно добавлены и закоммичены.")
+        print(f"{Colors.BLUE}Изменения успешно добавлены и закоммичены.{Colors.RESET}")
     except subprocess.CalledProcessError as e:
-        print(f"Ошибка при добавлении или коммите: {e}")
+        print(f"{Colors.RED}Ошибка при добавлении или коммите: {e}{Colors.RESET}")
 
 if __name__ == "__main__":
     # Путь к вашему скрипту
