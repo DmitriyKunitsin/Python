0d1615cbe0a3a7be357af00b3c1979bc00c3a74b
DmitryKunitsin <kunicind1996@gmail.com>
2024-09-03 16:39:07 +0700

    add status border-radius

--- PythonDesktop/Spectrometr/View/Menubar/menubar.py
+++ PythonDesktop/Spectrometr/View/Menubar/menubar.py
@@ -1,5 +1,6 @@
-from PyQt5.QtWidgets import QMenuBar, QAction, qApp
+from PyQt5.QtWidgets import QMenuBar, QAction, qApp, QLabel, QHBoxLayout, QWidget
 from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import Qt
 try:
     # View folder
     from View.Menubar.ConnectWindow.ConnectWIndow import ConnectWindow
@@ -16,23 +17,40 @@ import os
 exit_file_name_jpg = 'images/exit.jpg'
 connect_file_name_jpg = 'images/connect.jpg'
 setting_file_name_jpg = 'images/setting.jpg'
+disconnect_file_name_png = 'images/disconnect.png'
 
 class CustomMenuBar(QMenuBar):
 
     def __init__(self, parent=None, view_model=None):
         super().__init__(parent)
         self.view_model = view_model
-        self.initMenu()
 
+        # Создаем статусный виджет
+        self.status_widget = QWidget()
+        self.status_layout = QHBoxLayout(self.status_widget)
+        self.status_label = QLabel()
+        self.status_label.setFixedSize(20, 20)
+        self.update_status(False)
+
+        # Добавляем статусный кружок в layout
+        self.status_layout.addWidget(self.status_label)
+        self.status_layout.setAlignment(Qt.AlignRight)  # Выравнивание вправо
+        self.status_widget.setLayout(self.status_layout)
+
+        self.addWidgetToMenu(self.status_widget)
+
+        self.initMenu()
+    
+    def addWidgetToMenu(self, widget):
+        self.setCornerWidget(widget)
     def set_view_model(self, view_model):
         self.view_model = view_model
 
     def initMenu(self):
-
         file_menu = self.addMenu('Menu') # создаю объект менюшки
         if not os.path.isfile(exit_file_name_jpg):
             print(f"Файл не найден: {exit_file_name_jpg}")
-                    
+            
         # создаю действие
         exit_action = QAction(QIcon(exit_file_name_jpg), 'Exit', self)
         exit_action.setShortcut('Ctrl+Q') # Комбинация 
@@ -42,12 +60,6 @@ class CustomMenuBar(QMenuBar):
         file_menu.addAction(exit_action)
 
         connect_menu = self.addMenu('Device')
-
-        # # Загрузка и изменение размера изображения
-        # image = Image.open(connect_file_name_jpg)
-        # image = image.resize((1024, 1024))  # Задайте нужный размер
-        # image.save('images/resized_image.jpg')  # Сохраните изменённое изображение
-
         # Подключение к порту
         connect_action = QAction(QIcon(connect_file_name_jpg),'Подключиться', self)
         connect_action.setStatusTip('Подключиться к порту')
@@ -55,12 +67,11 @@ class CustomMenuBar(QMenuBar):
 
         connect_menu.addAction(connect_action)
 
-        disconect_port_action = QAction(QIcon(), 'Отключиться', self)
+        disconect_port_action = QAction(QIcon(disconnect_file_name_png), 'Отключиться', self)
         disconect_port_action.setStatusTip('Отключиться от порта')
         disconect_port_action.triggered.connect(self.disconect)
 
         connect_menu.addAction(disconect_port_action)
-
 
         setting_menu = self.addMenu('Setting')
 
@@ -71,18 +82,24 @@ class CustomMenuBar(QMenuBar):
         setting_menu.addAction(setting_uart_command)
 
     def disconect(self):
-        # self.set_view_model()
+        ''' Меняет булевое значение в цикле чтения порта'''
         if self.view_model:
             self.view_model.disconect_port()
+            self.update_status(False)
         else:
             print('self.view_model not found')
-
     def open_connect_window(self):
+        ''' Создает окно подключения к девайсу'''
+        self.update_status(True)
         self.connect_window = ConnectWindow(self.view_model)
         self.connect_window.show()
-    
     def open_setting_command_for_stm(self):
+        ''' Создает окно с настройками платы'''
         self.connect_setting_command = Setting_Command(self.view_model)
         self.connect_setting_command.show()
-
-
+    def update_status(self, is_connected):
+        """Обновляет статус подключения."""
+        if is_connected:
+            self.status_label.setStyleSheet("background-color: green; border-radius: 10px;")
+        else:
+            self.status_label.setStyleSheet("background-color: red; border-radius: 10px;")
