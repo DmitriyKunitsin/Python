c6369d13844b2c776d4106f08ef5964e3e0b686f
DmitriyKunitsin <kunicind1996@gmail.com>
2024-09-30 21:23:07 +0700

    add less 8

--- Magistatura/Labs 4/lesson_8.py
+++ Magistatura/Labs 4/lesson_8.py
@@ -6,7 +6,6 @@ def main():
     df = pd.read_table('wr88125.txt', delimiter=';')
     df.columns = ['index', 'year', 'month', 'day', 'min_t', 'average_t', 'max_t', 'fainfall']
 
-        # столбцы в числовые значения, заменяя ошибки на NaN
     df['min_t'] = pd.to_numeric(df['min_t'], errors='coerce')
     df['average_t'] = pd.to_numeric(df['average_t'], errors='coerce')
     df['max_t'] = pd.to_numeric(df['max_t'], errors='coerce')
