efae8d0f9e9f8f87e42cd499765d3777ac16dce5
DmitryKunitsin <kunicind1996@gmail.com>
2024-10-16 17:29:40 +0700

    add less 8

--- Magistatura/Labs9_1/lesson_8.py
+++ Magistatura/Labs9_1/lesson_8.py
@@ -4,6 +4,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import PolynomialFeatures
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
@@ -28,47 +29,106 @@ def main():
     scaler = StandardScaler()
     X_scaled = scaler.fit_transform(X)
 
-    # Определение дисперсии и выбор 5 признаков с набольшей дисперсией 
-    variance = np.var(X_scaled, axis=0)
-    top_variance_indices = np.argsort(variance)[-5:]
-    top_variance_features = X.columns[top_variance_indices]
-    print("5 признаков с наибольшей дисперсией:")
-    # Дисперсия Это отслеживание признаков, чьи значения чаще всего изменяются
-    print(top_variance_features)
+    # # Определение дисперсии и выбор 5 признаков с набольшей дисперсией 
+    # variance = np.var(X_scaled, axis=0)
+    # top_variance_indices = np.argsort(variance)[-5:]
+    # top_variance_features = X.columns[top_variance_indices]
+    # print("5 признаков с наибольшей дисперсией:")
+    # # Дисперсия Это отслеживание признаков, чьи значения чаще всего изменяются
+    # print(top_variance_features)
 
-    # Определение корреляции и выбор 5 признаков с наибольшей корреляцией с целевым столбцогм
-    correlation = df_numeric.corr()
-    top_correlation_features = correlation['SalePrice'].nlargest(6).index[1:] # Исключаем SalePrice 
-    print("5 признаков с наибольшей корреляцией с целевым столбцом:")
-    # Корелиаци это связь между признаками, чем выше значение, тем сильнее связь
-    print(top_correlation_features)
-
-    # Стандартизация целевого столбца 
-    y_scaled = scaler.fit_transform(y.values.reshape(-1, 1)).flatten()
+    # # Определение корреляции и выбор 5 признаков с наибольшей корреляцией с целевым столбцогм
+    # correlation = df_numeric.corr()
+    # top_correlation_features = correlation['SalePrice'].nlargest(6).index[1:] # Исключаем SalePrice 
+    # print("5 признаков с наибольшей корреляцией с целевым столбцом:")
+    # # Корелиаци это связь между признаками, чем выше значение, тем сильнее связь
+    # print(top_correlation_features)
 
     # Разделение на обучающую и тестовую выборку
     X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.3, random_state=42)
 
-    print('Размер обучающей выборки: ', X_train.shape)
-    print(f'Размер тестовой выборки: {X_test.shape}')
+    degrees = [1,2,3,4,5]
+    results = []
+    for degree in degrees:
 
-    # Обучение модели линейной регрессии
-    model = LinearRegression()
-    model.fit(X_train, y_train)
+        poly_features = PolynomialFeatures(degree=degree)
+        # Здесь создаются полиномиальные признаки. 
+        # Это означается, что к исходным признакам добавляются их полиномиальные комбинации до указанной степени. 
+        # Например, если есть два признака x1 и x2 , то при степени 2 будут добавилены  x₁², x₂² и x₁ ⋅ x₂.
+        x_poly_train = poly_features.fit_transform(X_train)
+        # fit_transform применяется к обучающей выборке и создает новые полномиальные признаки
+        x_poly_test = poly_features.transform(X_test)
+        # transform применяется к тестовой выборке и создает те же полиномиальные признаки, но без их обучения(т.е. без изменения параметров)
 
-    # Предсказание
-    y_pred = model.predict(X_test)
 
-    # Оценка качества модели
-    score = r2_score(y_test, y_pred)
-    mae = mean_absolute_error(y_test, y_pred)
-    mse = mean_squared_error(y_test, y_pred)
+        model = LinearRegression()
+        # Здесь создается объект линейной регрессии и обучается 
+        model.fit(x_poly_train, y_train)
+        # на полиномиальных признаках из обучающей выборки(x_poly_train) и соответствующих целевых значениях(y_train). 
+        # Функция fit обучаается модель, подбирая коэффициенты для линейной регрессии 
+
+
+        # Функция predict используется для получения предсказаний
+        y_pred = model.predict(x_poly_test)
+        # на основе тестовой выборки с полиномильными признаками (x_poly_test)
+        # Она возвращает предсказанные значения целевой переменной (y_pred)
+
+        # Оценка качества модели
+        score = r2_score(y_test, y_pred)
+        mae = mean_absolute_error(y_test, y_pred)
+        mse = mean_squared_error(y_test, y_pred)
+        rmse = np.sqrt(mse)
+        
+        results.append((degree, score, mae, mse, rmse))
+
+    # Вывод результатов
+    for degree, score, mae, mse, rmse in results:
+        print(f'Степень: {degree}, R²: {score}, MAE: {mae}, MSE: {mse}, RMSE: {rmse}')
+    
+    # Построение графика валидационной кривой
+    degrees, scores = zip(*[(degree, score) for degree, score, _, _, _ in results])
+        
+    plt.plot(degrees, scores)
+    plt.title("Валидационная кривая")
+    plt.xlabel("Степень полинома")
+    plt.ylabel("R²")
+    plt.xticks(degrees)
+    plt.show()
+
+    result_features = PolynomialFeatures(degree=1)
+    x_poly_train = result_features.fit_transform(X_train)
+    x_poly_test = result_features.transform(X_test)
+    
+    result_model = LinearRegression()
+    result_model.fit(x_poly_train, y_train)
+
+    y_pred_result = result_model.predict(x_poly_test)
+
+    score = r2_score(y_test, y_pred_result)
+    mae = mean_absolute_error(y_test, y_pred_result)
+    mse = mean_squared_error(y_test, y_pred_result)
     rmse = np.sqrt(mse)
+    
+    print(f'Оценка качества модели с оптимальной степенью полинома: \nR² : {score}\n MAE : {mae}\n MSE: {mse} \n RMSE: {rmse}')
 
-    print(f'R²: {score}')
-    print(f'MAE: {mae}')
-    print(f'MSE: {mse}')
-    print(f'RMSE: {rmse}')
+    data = pd.DataFrame({
+        'Оригинальные цены' : y_test,
+        'Предсказанные цены' : y_pred_result
+    })
+    plt.figure(figsize=(10,10))
+    sb.scatterplot(x='Оригинальные цены', y='Предсказанные цены', data=data)
+    plt.plot([data['Оригинальные цены'].min(), data['Оригинальные цены'].max()],
+         [data['Оригинальные цены'].min(), data['Оригинальные цены'].max()],
+         color='red', linestyle='--')
+   
+    plt.title('Сравнение оригинальных и предсказанных цен')
+    plt.xlabel('Оригинальные цены')
+    plt.ylabel('Предсказанные цены')
+    plt.xlim(data['Оригинальные цены'].min(), data['Оригинальные цены'].max())
+    plt.ylim(data['Предсказанные цены'].min(), data['Предсказанные цены'].max())
+    plt.grid()
+    plt.show()
+
 
 if __name__ == '__main__':
     main()
